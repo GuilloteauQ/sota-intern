60037cb49c4428c5bdc129f99e1461b1d85cc60f
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-08-24 16:52:55 +0200

    nix pkg

--- flake.nix
+++ flake.nix
@@ -1,23 +1,25 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.05";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/23.05"; };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in
-    {
+    in {
 
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            go
-          ];
+      packages.${system} = rec {
+        default = sota-intern;
+        sota-intern = pkgs.buildGoModule {
+          name = "sota-intern";
+          version = "0.0";
+          src = ./sota-intern;
+          vendorSha256 = "sha256-7PswrA6eVste+nYpoq93o4Jnly5dW7y2J/dOYQVaAh4=";
         };
+      };
+      devShells.${system} = {
+        default = pkgs.mkShell { buildInputs = with pkgs; [ go ]; };
       };
 
     };
